fb313845e95d486643380d94bfd6430bb0f8e71c
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2026-03-04 08:41:45 -0500

    add score tests, all failing

---
+++ test_score.py
@@ -0,0 +1,13 @@
+import pytest
+from score import *
+def test_add_points(game,amount):
+    pass
+
+def test_apply_multiplier(game, multiplier):
+    pass
+
+def test_reset_score(game):
+    pass
+
+def test_is_high_score(game, threshold):
+    pass
