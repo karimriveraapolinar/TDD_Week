32d9906a11c03fb0d8863fb1b41abf58688b561c
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2026-03-05 20:43:38 -0500

    added remove_item function, all tests passed

--- inventory.py
+++ inventory.py
@@ -2,7 +2,12 @@ def add_item(inventory, item):
     pass
 
 def remove_item(inventory, item):
-    pass
+    if item not in inventory["items"]:
+        raise ValueError("That item isn't in your inventory")
+    if inventory["locked"] is True:
+        return inventory
+    inventory["items"].pop(inventory["items"].index(item))
+    return inventory
 
 def get_item_count(inventory):
     return len(inventory["items"])
